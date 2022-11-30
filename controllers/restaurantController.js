ee0a3b07774f1cdcdb739a40c476957b5acfa41b
VSS <vss@VSSs-Mac-mini.local>
2022-11-30 17:43:12 +0700

    restaurant modal add properties

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,69 +1,92 @@
 const restaurantModel = require("../models/restaurant");
 
 const restaunrantController = {
-    createRestaurant: async (req, res) => {
-        try {
-            const { name, address_detail, address, open_time, close_time, images, price, rate } = req.body;
-            const restaurantCreate = await new restaurantModel({
-                name,
-                address_detail,
-                address, 
-                open_time,
-                close_time, images,
-                price,
-                rate
-            }).save();
-            console.log('response restaunrant', restaurantCreate);
-            res.status(200).json('tao thanh cong')
-        
-        } catch (error) {
-            console.log('error restaurant', error);
-            res.status(500).json(error)
-        }
-    },
-
-    getAllRestaurant: async (req, res) => {
-        try {
-            const response = await restaurantModel.find();
-            res.status(200).json(response);
-        } catch (error) {
-            res.status(500).json('ko get duoc du lieu restaurant')
-        }
-    },
-
-    updateRestaurant: async (req, res) => {
-        try {
-            
-            const {id} = req.params;
-            const { name, address_detail, address, open_time, close_time, images, price, rate } = req.body;
-
-            await restaurantModel.findOneAndUpdate({idrestaurant: id}, {
-                name,
-                address_detail,
-                address,
-                open_time,
-                close_time,
-                images,
-                price,
-                rate
-            }, {new: true})
-            res.status(200).json('cap nhat nha hang thanh cong')
-
-        } catch (error) {
-            res.status(500).json('cap nhat nha hang loi')
-        }
-    },
-
-    deleteRestaurant: async (req, res) => {
-        try {
-            const {id} = req.params;
-            await restaurantModel.findByIdAndDelete({_id: id});
-            res.status(200).json('xoa nha hang thanh cong');
-        } catch (error) {
-            res.status(500).json('xoa nha hang that bai')
-        }
+  createRestaurant: async (req, res) => {
+    try {
+      const {
+        name,
+        address_detail,
+        address,
+        open_time,
+        close_time,
+        images,
+        price,
+        rate,
+        description,
+      } = req.body;
+      const restaurantCreate = await new restaurantModel({
+        name,
+        address_detail,
+        address,
+        open_time,
+        close_time,
+        images,
+        price,
+        rate,
+        description,
+      }).save();
+      console.log("response restaunrant", restaurantCreate);
+      res.status(200).json("tao thanh cong");
+    } catch (error) {
+      console.log("error restaurant", error);
+      res.status(500).json(error);
     }
-}
+  },
 
+  getAllRestaurant: async (req, res) => {
+    try {
+      const response = await restaurantModel.find();
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(500).json("ko get duoc du lieu restaurant");
+    }
+  },
+
+  updateRestaurant: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const {
+        name,
+        address_detail,
+        address,
+        open_time,
+        close_time,
+        images,
+        price,
+        rate,
+        description,
+      } = req.body;
+
+      await restaurantModel.findOneAndUpdate(
+        { idrestaurant: id },
+        {
+          name,
+          address_detail,
+          address,
+          open_time,
+          close_time,
+          images,
+          price,
+          rate,
+          description,
+        },
+        { new: true }
+      );
+      res.status(200).json("cap nhat nha hang thanh cong");
+    } catch (error) {
+      res.status(500).json("cap nhat nha hang loi");
+    }
+  },
+
+  deleteRestaurant: async (req, res) => {
+    try {
+      const { id } = req.params;
+      await restaurantModel.findByIdAndDelete({ _id: id });
+      res.status(200).json("xoa nha hang thanh cong");
+    } catch (error) {
+      res.status(500).json("xoa nha hang that bai");
+    }
+  },
+};
 
 module.exports = restaunrantController;
--- models/restaurant.js
+++ models/restaurant.js
@@ -1,34 +1,37 @@
 const mongoose = require("mongoose");
-const AutoIncrement = require('mongoose-sequence')(mongoose);
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 const imageSchema = new mongoose.Schema({
-    image: String
-})
+  image: String,
+});
 
 
+//theem truong description vao restaurant
 const restaurantSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 6,
-        maxlength: 500,
-        unique: true
-    },
-    // detail_address: {
-    //     type: String,
-    //     required: true,
-    // },
-    address: {
-        type: String,
-        required: true
-    },
-    address_detail: String,
-    open_time: String,
-    close_time: String,
-    images: [imageSchema],
-    price: Number,
-    rate: String
-})
+  name: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 500,
+    unique: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  address_detail: String,
+  open_time: String,
+  close_time: String,
+  images: [imageSchema],
+  price: Number,
+  rate: String,
+  description: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 500,
+  },
+});
 
-restaurantSchema.plugin(AutoIncrement,{inc_field: 'idrestaurant'})
+restaurantSchema.plugin(AutoIncrement, { inc_field: "idrestaurant" });
 module.exports = mongoose.model("Restaurant", restaurantSchema);
