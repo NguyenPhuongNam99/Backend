4da834d67dfc1b5f7897c433a21d615e7e30699e
VSS <vss@VSSs-Mac-mini.local>
2022-12-05 11:50:41 +0700

    file upload

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const userRoute = require("./routes/user");
 const tour = require("./routes/tour");
 const voucher = require("./routes/voucher");
 const hotel = require("./routes/hotel");
-const restaunrant = require('./routes/restaurant');
-const order_tour = require('./routes/orderTour');
-const review = require('./routes/review');
-const room = require('./routes/room');
-const tourSchedule = require('./routes/tourSchedule');
-const upload = require('./routes/upload')
+const restaunrant = require("./routes/restaurant");
+const order_tour = require("./routes/orderTour");
+const review = require("./routes/review");
+const room = require("./routes/room");
+const tourSchedule = require("./routes/tourSchedule");
+const upload = require("./routes/upload");
 
 // const conn = mongoose.connection;
 // conn.once("open", function () {
@@ -23,38 +23,60 @@ const upload = require('./routes/upload')
 //   gfs.collection("photos");
 // })
 
-
 dotenv.config();
 
 mongoose.connect(
-  'mongodb+srv://namnguyen:NGUYENphuongnam1010@atlascluster.cnc8ipm.mongodb.net/?retryWrites=true&w=majority',{
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 30000,
-  socketTimeoutMS: 75000,
-  keepAlive: true,
-},
+  "mongodb+srv://namnguyen:NGUYENphuongnam1010@atlascluster.cnc8ipm.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000,
+    socketTimeoutMS: 75000,
+    keepAlive: true,
+    useNewUrlParser: true,
+  },
   (err) => {
-   if(err) console.log('error new', err) 
-   else console.log("mongdb is connected");
+    if (err) console.log("error new", err);
+    else console.log("mongdb is connected");
   }
 );
 const app = express();
+// var appView = app.createServer();
+
+// var conditionalCSRF = function (req, res, next) {
+//   //compute needCSRF here as appropriate based on req.path or whatever
+//   if (needCSRF) {
+//     csrf(req, res, next);
+//   } else {
+//     next();
+//   }
+// }
+
+// app.use(conditionalCSRF);
 
 let gfs;
 
 const conn = mongoose.connection;
 conn.once("open", function () {
-    gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection("photos");
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("photos");
 });
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    // optionSuccessStatus: 200,
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    // allowedHeaders: "Authorization,Origin,X-Requested-With,Content-Type,Accept",
+  })
+);
 
 app.use("/file", upload);
 
-app.use(cors({
-    credentials: true,
-    origin: "http://localhost:3000",
-}));
-
+// app.use(cors({
+//     credentials: true,
+//     origin: "http://localhost:3000",
+// }));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -73,38 +95,34 @@ app.use("/v1/tourSchedule", tourSchedule);
 
 // media routes
 app.get("/file/:filename", async (req, res) => {
-    try {
-        const file = await gfs.chunks.find({ filename: req.params.filename });
-        const readStream = await gfs.createReadStream(file.filename);
-        readStream.pipe(res);
-    } catch (error) {
-        res.send("not found");
-    }
+  try {
+    const file = await gfs.chunks.find({ filename: req.params.filename });
+    gfs.createReadStream(file.filename).pipe(res);
+  } catch (error) {
+    res.send("not found");
+  }
 });
 
 app.delete("/file/:filename", async (req, res) => {
-    try {
-        await gfs.files.deleteOne({ filename: req.params.filename });
-        res.send("success");
-    } catch (error) {
-        console.log(error);
-        res.send("An error occured.");
-    }
+  try {
+    await gfs.files.deleteOne({ filename: req.params.filename });
+    res.send("success");
+  } catch (error) {
+    console.log(error);
+    res.send("An error occured.");
+  }
 });
-
 
 app.listen(8000, () => {
   console.log("server is running");
 });
 
-
-
 //1     user admin vào tạo tour ==> chọn cả khách sạn và nhà hàng ==> tổng giá
 
 //created by ?
-//thêm tác nhân là nhân viên  ==> nhân viên sẽ là người tạo tour ==> admin là người phê duyệt 
+//thêm tác nhân là nhân viên  ==> nhân viên sẽ là người tạo tour ==> admin là người phê duyệt
 
-//      danh sách hiển thị tour ==> chỉ hiển 1 ảnh đại diện cho địa điểm đó ==> 
+//      danh sách hiển thị tour ==> chỉ hiển 1 ảnh đại diện cho địa điểm đó ==>
 
 //2     nhà hàng, khách sạn
 //nhà hàng với khách sạn vào tạo nhà hàng, ks cho chính mình ==> thêm sửa xoá
@@ -114,11 +132,8 @@ app.listen(8000, () => {
 //4     mục tab menu icon
 // ==> khách hàng ko muốn chọn tour có thể chọn nhà hàng hoặc khách sạn theo ý thích của riêng mình
 
-
-
-
-//5    role: nhân viên ==> username, lastname, first name, role: employee, 
-//bình thường sẽ là đăng ký mặc định là user 
-//admin sẽ mặc định có tài khoản 
+//5    role: nhân viên ==> username, lastname, first name, role: employee,
+//bình thường sẽ là đăng ký mặc định là user
+//admin sẽ mặc định có tài khoản
 
 //admin sẽ tạo tài khoản cho employee
--- routes/upload.js
+++ routes/upload.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/upload", upload.single("file"), async (req, res) => {
+    
     if (req.file === undefined) return res.send("you must select a file.");
     const imgUrl = `http://localhost:8000/file/${req.file.filename}`;
     return res.send(imgUrl);
